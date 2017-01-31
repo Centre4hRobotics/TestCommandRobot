779ad135369f35b403cbcedb3b6cec1251596fd3
FlyingSpy <elijah.johnson2019@gmail.com>
2017-01-31 18:40:53 -0500

    StopEverything Command

--- src/OperatorInterface.cpp
+++ src/OperatorInterface.cpp
@@ -2,6 +2,7 @@
 #include <Joystick.h>
 
 #include "Commands/TurnAndSeek.h"
+#include "Commands/StopEverything.h"
 
 OperatorInterface::OperatorInterface()
 {
@@ -9,4 +10,8 @@ OperatorInterface::OperatorInterface()
 	Button_X = new JoystickButton(_joystick, 3);
 
 	Button_X->WhenPressed(new TurnAndSeek());
+
+	Button_Y = new JoystickButton(_joystick, 4);
+
+	Button_Y->WhenPressed(new StopEverything());
 }
